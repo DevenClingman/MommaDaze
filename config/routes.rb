51b56959630c31af0d451e521d4d2e022f019338
Deven Clingman <dclingy@gmail.com>
2017-09-19 11:19:19 -0600

    added nav partial and added home and about page

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
+  get 'home', to: 'pages#home'
+
+  get 'about', to: 'pages#about'
+
   resources :posts
   resources :blogs
 
-  root to: 'blogs#index'
+  root to: 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
