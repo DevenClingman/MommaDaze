bf669b95ef0678f285b8f431e3caaf42a23ee558
Deven Clingman <dclingy@gmail.com>
2017-09-25 11:34:22 -0600

    completed implementation of authorization with petergate gem

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,9 +1,11 @@
 module ApplicationHelper
   def auth_links
-    if user_signed_in?
-      link_to "Logout", destroy_user_session_path, method: :delete
+    if current_user.is_a?(GuestUser)
+      (link_to "Register", new_user_registration_path) + 
+      "<br>".html_safe +
+      (link_to "Login", user_session_path)
     else
-      (link_to "Register", new_user_registration_path) + (link_to "Login", user_session_path)
+      link_to "Logout", destroy_user_session_path, method: :delete
     end 
   end
 end
