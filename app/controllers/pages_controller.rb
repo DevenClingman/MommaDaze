d5ef1bdab3d94977164b9d70739f04c54fa809fd
Deven Clingman <dclingy@gmail.com>
2017-10-01 18:07:54 -0600

    implemented minor changes to about, footer, and nav pages. Also added most dazed page:

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -4,4 +4,7 @@ class PagesController < ApplicationController
 
   def about
   end
+
+  def mostdazed
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,10 +3,11 @@ Rails.application.routes.draw do
   get 'home', to: 'pages#home'
 
   get 'about', to: 'pages#about'
+  get 'most_dazed', to: 'pages#mostdazed'
 
   resources :posts
   resources :blogs
 
-  root to: 'pages#home'
+  root to: 'posts#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
