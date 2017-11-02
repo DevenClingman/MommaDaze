7986a9a3e8cc1f03760efdf23347646d09c9197f
Deven Clingman <dclingy@gmail.com>
2017-11-01 19:43:08 -0600

    added slugs to posts

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,9 @@
 class Post < ApplicationRecord
   enum status: { submitted: 0, approved: 1 }
+
+  extend FriendlyId
+  friendly_id :title, use: :slugged
+
   belongs_to :user
   validates_presence_of :title, :description, :user_id
 
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171023035015) do
+ActiveRecord::Schema.define(version: 20171102013834) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -46,6 +46,8 @@ ActiveRecord::Schema.define(version: 20171023035015) do
     t.datetime "updated_at", null: false
     t.bigint "user_id"
     t.integer "status", default: 0
+    t.string "slug"
+    t.index ["slug"], name: "index_posts_on_slug", unique: true
     t.index ["user_id"], name: "index_posts_on_user_id"
   end
 
